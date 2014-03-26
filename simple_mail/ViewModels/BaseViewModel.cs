1f8a6b50fae09694b2c26a44a397876e6c3b9d31
Yura95st <yura95st@gmail.com>
2014-03-26 16:48:15 +0200

    adds BaseViewModel class

---
+++ simple_mail/ViewModels/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using simple_mail.HelperClasses;
+
+namespace simple_mail.ViewModels
+{
+    /// <summary>
+    /// Provides common functionality for ViewModel classes
+    /// </summary>
+    public abstract class BaseViewModel : ObservableObject
+    { }
+}
