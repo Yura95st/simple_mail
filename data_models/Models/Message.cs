c15cf1163acca5559efeee0c1d06c19395f9b5fd
Yura95st <yura95st@gmail.com>
2014-03-26 14:59:44 +0200

    adds Message and User model classes

---
+++ data_models/Models/Message.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+namespace data_models.Models
+{
+    public class Message
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Subject
+        {
+            get;
+            set;
+        }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+
+        public DateTime PubDate
+        {
+            get;
+            set;
+        }
+
+        public int State
+        {
+            get;
+            set;
+        }
+
+        public User Author
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ data_models/Models/User.cs
@@ -0,0 +1,35 @@
+﻿
+namespace data_models.Models
+{
+    public class User
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+        public int State
+        {
+            get;
+            set;
+        }
+    }
+}
