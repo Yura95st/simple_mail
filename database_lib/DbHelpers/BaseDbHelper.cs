137798fdde512bbd323246e996438d29f4046503
Yura95st <yura95st@gmail.com>
2014-03-26 15:18:15 +0200

    adds BaseDbHelper

---
+++ database_lib/DbHelpers/BaseDbHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace database_lib.DbHelpers
+{
+    public abstract class BaseDbHelper
+    {
+        protected SqlConnection connection;
+
+        public void SetConnectionString(string connectionString)
+        {
+            connection = new SqlConnection();
+            this.connection.ConnectionString = connectionString;
+        }
+    }
+}
