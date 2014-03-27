4910064fa452027a45db039221600a71c19d8463
Yura95st <yura95st@gmail.com>
2014-03-27 16:55:50 +0200

    implements Registration and Authorization process

--- simple_mail/ViewModels/Authorization/AuthorizationViewModel.cs
+++ simple_mail/ViewModels/Authorization/AuthorizationViewModel.cs
@@ -1,9 +1,99 @@
-﻿
+﻿using data_models.Validations;
+using data_models.Models;
+using database_lib.DbHelpers;
+using simple_mail.HelperClasses;
+using simple_mail.Models;
+using System.Windows.Input;
+using System;
+
 namespace simple_mail.ViewModels
 {
     public class AuthorizationViewModel : BaseViewModel
     {
+        private UserModel _userModel = new UserModel();
+        private ICommand _signInCommand;
+        private string _signInInfoMsg = "";
+
         public AuthorizationViewModel()
         { }
+
+        public UserModel UserAccount
+        {
+            get
+            {
+                return _userModel;
+            }
+            set
+            {
+                if (value != _userModel)
+                {
+                    _userModel = value;
+                    OnPropertyChanged("UserAccount");
+                }
+            }
+        }
+
+        public string SignInInfoMsg
+        {
+            get
+            {
+                return _signInInfoMsg;
+            }
+            set
+            {
+                if (value != _signInInfoMsg)
+                {
+                    _signInInfoMsg = value;
+                    OnPropertyChanged("SignInInfoMsg");
+                }
+            }
+        }
+
+        public ICommand SignInCommand
+        {
+            get
+            {
+                if (_signInCommand == null)
+                {
+                    _signInCommand = new RelayCommand(
+                        param => AuthorizeUser(),
+                        param => AreValidUserFields()
+                    );
+                }
+                return _signInCommand;
+            }
+        }
+
+        private void AuthorizeUser()
+        {
+            SignInInfoMsg = "";
+
+            if (!MyValidation.IsValidEmail(UserAccount.Email))
+            {
+                SignInInfoMsg = "INFO: Email is invalid!";
+                return;
+            }
+
+            UserDbHelper userDbHelper = new UserDbHelper();
+            User userInDb = null;
+            
+            try 
+            {                
+                userInDb = userDbHelper.GetUserByEmail(UserAccount.Email);
+            }
+            catch(Exception e)
+            {}
+
+            if (!String.Equals(userInDb.Password, MyValidation.Hash(UserAccount.Password, userInDb.Email)))
+            {
+                SignInInfoMsg = "INFO: Password is invalid!";
+                return;
+            }
+        }
+
+        private bool AreValidUserFields()
+        {
+            return !(string.IsNullOrEmpty(UserAccount.Email) || string.IsNullOrEmpty(UserAccount.Password));
+        }
     }
 }
--- simple_mail/ViewModels/Authorization/RegistrationViewModel.cs
+++ simple_mail/ViewModels/Authorization/RegistrationViewModel.cs
@@ -1,9 +1,134 @@
-﻿
+﻿using data_models.Models;
+using data_models.Validations;
+using database_lib.DbHelpers;
+using simple_mail.HelperClasses;
+using simple_mail.Models;
+using System;
+using System.Windows.Input;
+
 namespace simple_mail.ViewModels
 {
     public class RegistrationViewModel : BaseViewModel
     {
+        private UserModel _userModel = new UserModel();
+        private ICommand _signUpCommand;
+        private string _confirmPassword = "";
+        private string _signUpInfoMsg = "";
+
         public RegistrationViewModel()
-        { }
+        {
+            UserAccount.Email = "yura95st@gmail.com";
+            UserAccount.FirstName = "Yura";
+            UserAccount.Password = "1111";
+        }
+
+        public UserModel UserAccount
+        {
+            get
+            {
+                return _userModel;
+            }
+            set
+            {
+                if (value != _userModel)
+                {
+                    _userModel = value;
+                    OnPropertyChanged("UserAccount");
+                }
+            }
+        }
+
+        public string ConfirmPassword
+        {
+            get
+            {
+                return _confirmPassword;
+            }
+            set
+            {
+                if (value != _confirmPassword)
+                {
+                    _confirmPassword = value;
+                    OnPropertyChanged("ConfirmPassword");
+                }
+            }
+        }
+
+        public string SignUpInfoMsg
+        {
+            get
+            {
+                return _signUpInfoMsg;
+            }
+            set
+            {
+                if (value != _signUpInfoMsg)
+                {
+                    _signUpInfoMsg = value;
+                    OnPropertyChanged("SignUpInfoMsg");
+                }
+            }
+        }
+
+        public ICommand SignUpCommand
+        {
+            get
+            {
+                if (_signUpCommand == null)
+                {
+                    _signUpCommand = new RelayCommand(
+                        param => CreateNewUserAccount(),
+                        param => AreValidUserFields()
+                    );
+                }
+                return _signUpCommand;
+            }
+        }
+
+        private void CreateNewUserAccount()
+        {
+            SignUpInfoMsg = "";
+
+            if (!MyValidation.IsValidEmail(UserAccount.Email))
+            {
+                SignUpInfoMsg = "INFO: Email is invalid!";
+                return;
+            }
+
+            if (!MyValidation.IsValidFirstName(UserAccount.FirstName))
+            {
+                SignUpInfoMsg = "INFO: UserName is invalid!";
+                return;
+            }
+
+            if (!ArePasswordEqual())
+            {
+                SignUpInfoMsg = "INFO: Passwords are not equal!";
+                return;
+            }
+
+            UserDbHelper userDbHelper = new UserDbHelper();
+
+            try
+            {
+                int newUserId = userDbHelper.AddNewUser(UserAccount.User);
+                //TODO: Set user online
+            }
+            catch (Exception e)
+            {
+                SignUpInfoMsg = "INFO: UserAccount with such email already exists!";
+            }
+        }
+
+        private bool AreValidUserFields()
+        {
+            return !(string.IsNullOrEmpty(UserAccount.FirstName) || string.IsNullOrEmpty(UserAccount.Email) || string.IsNullOrEmpty(UserAccount.Password) 
+                || string.IsNullOrEmpty(this.ConfirmPassword));
+        }
+
+        private bool ArePasswordEqual()
+        {
+            return (string.Equals(UserAccount.Password, ConfirmPassword));
+        }
     }
 }
