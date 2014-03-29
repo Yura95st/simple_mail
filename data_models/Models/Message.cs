6967c6f8adf550fe68be833fe466a147f3d99b3c
Yura95st <yura95st@gmail.com>
2014-03-29 17:44:18 +0200

    updates MessageModel

--- data_models/Models/Message.cs
+++ data_models/Models/Message.cs
@@ -1,43 +1,115 @@
-﻿
+﻿using data_models.HelperClasses;
+using data_models.Models;
 using System;
+
 namespace data_models.Models
 {
-    public class Message
+    public class Message : ObservableObject
     {
+        private int _id;
+        private string _subject;
+        private string _text;
+        private DateTime _pubDate;
+        private User _author;
+        private int _state;
+
+        public Message()
+        { }
+
         public int Id
         {
-            get;
-            set;
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
         }
 
         public string Subject
         {
-            get;
-            set;
+            get
+            {
+                return _subject;
+            }
+            set
+            {
+                if (value != _subject)
+                {
+                    _subject = value;
+                    OnPropertyChanged("Subject");
+                }
+            }
         }
 
         public string Text
         {
-            get;
-            set;
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                if (value != _text)
+                {
+                    _text = value.ToLower();
+                    OnPropertyChanged("Text");
+                }
+            }
         }
 
         public DateTime PubDate
         {
-            get;
-            set;
-        }
-
-        public int State
-        {
-            get;
-            set;
+            get
+            {
+                return _pubDate;
+            }
+            set
+            {
+                if (value != _pubDate)
+                {
+                    _pubDate = value;
+                    OnPropertyChanged("PubDate");
+                }
+            }
         }
 
         public User Author
         {
-            get;
-            set;
+            get
+            {
+                return _author;
+            }
+            set
+            {
+                if (value != _author)
+                {
+                    _author = value;
+                    OnPropertyChanged("Author");
+                }
+            }
+        }
+
+        public int State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                if (value != _state)
+                {
+                    _state = value;
+                    OnPropertyChanged("State");
+                }
+            }
         }
     }
 }
