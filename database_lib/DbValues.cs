a35405f71e27c2fc1aee01c416a1d4bcf572b859
Yura95st <yura95st@gmail.com>
2014-03-30 02:13:59 +0200

    add messages_view values in DbValue

--- database_lib/DbValues.cs
+++ database_lib/DbValues.cs
@@ -6,6 +6,7 @@
         public static string TABLE_USERS = "users";
         public static string TABLE_MESSAGES = "messages";
         public static string TABLE_MESSAGE_USERS = "message_users";
+        public static string TABLE_MESSAGES_VIEW = "messages_view";
 
         // Table Caption Names
         public static string TABLE_CAPTION_USERS = "u";
@@ -45,6 +46,7 @@
         public static string MESSAGES_CAPTION_COLUMN_STATE = TABLE_MESSAGES + "_" + MESSAGES_COLUMN_STATE;
 
 
+
         // MESSAGE_USERS table - column names
         public static string MESSAGE_USERS_COLUMN_ID = "msg_id";
         public static string MESSAGE_USERS_COLUMN_USER_ID = "user_id";
@@ -54,5 +56,35 @@
         public static string MESSAGE_USERS_CAPTION_COLUMN_ID = TABLE_MESSAGE_USERS + "_" + MESSAGE_USERS_COLUMN_ID;
         public static string MESSAGE_USERS_CAPTION_COLUMN_USER_ID = TABLE_MESSAGE_USERS + "_" + MESSAGE_USERS_COLUMN_USER_ID;
         public static string MESSAGE_USERS_CAPTION_COLUMN_STATE = TABLE_MESSAGE_USERS + "_" + MESSAGE_USERS_COLUMN_STATE;
+
+
+        // MESSAGES_VIEW view - column names
+        public static string MESSAGES_VIEW_COLUMN_ID = "messages_msg_id";
+        public static string MESSAGES_VIEW_COLUMN_SUBJECT = "messages_subject";
+        public static string MESSAGES_VIEW_COLUMN_TEXT = "messages_text";
+        public static string MESSAGES_VIEW_COLUMN_PUB_DATE = "messages_pub_date";
+        public static string MESSAGES_VIEW_COLUMN_STATE = "messages_state";
+        public static string MESSAGES_VIEW_COLUMN_RECIPIENT_MSG_STATE = "messages_recipient_msg_state";
+        public static string MESSAGES_VIEW_COLUMN_AUTHOR_ID = "author_id";
+        public static string MESSAGES_VIEW_COLUMN_AUTHOR_FIRST_NAME = "author_first_name";
+        public static string MESSAGES_VIEW_COLUMN_AUTHOR_EMAIL = "author_email";
+        public static string MESSAGES_VIEW_COLUMN_AUTHOR_STATE = "author_state";
+        public static string MESSAGES_VIEW_COLUMN_RECIPIENT_ID = "recipient_user_id";
+        public static string MESSAGES_VIEW_COLUMN_RECIPIENT_FIRST_NAME = "recipient_first_name";
+        public static string MESSAGES_VIEW_COLUMN_RECIPIENT_EMAIL = "recipient_email";
+        public static string MESSAGES_VIEW_COLUMN_RECIPIENT_STATE = "recipient_state";
+
+
+        // MESSAGES_VIEW select query:
+        //    SELECT m.msg_id, m.subject, m.text, m.pub_date, m.state, 
+        //        a.user_id AS author_id, a.first_name AS author_first_name, a.email AS author_email, a.state AS author_state,
+        //        u.user_id AS recipient_id, u.first_name AS recipient_first_name, u.email AS recipient_email, u.state AS recipient_state,
+        //        mu.state AS recipient_msg_state
+        //    FROM messages m JOIN users a
+        //        ON m.author_id = a.user_id
+        //    JOIN message_users mu
+        //        ON mu.msg_id = m.msg_id
+        //    JOIN users u
+        //        ON u.user_id = mu.user_id
     }
 }
