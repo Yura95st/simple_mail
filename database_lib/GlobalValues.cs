87f6442198eb1a7dd21e95d45e38db4c2dca78b7
Yura95st <yura95st@gmail.com>
2014-04-02 23:46:19 +0300

    adds EntityModel to database_lib project modifies App.config

---
+++ database_lib/GlobalValues.cs
@@ -0,0 +1,16 @@
+﻿using database_lib.DbHelpers;
+
+namespace database_lib
+{
+    public static class GlobalValues
+    {
+        public static int HelpersTechnologyType
+        {
+            get 
+            {
+                return (int)DbHelpersFactory.HelperTechTypes.SimpleDataReader;
+                //return (int)DbHelpersFactory.HelperTechTypes.EntityFramework;
+            }
+        }
+    }
+}
